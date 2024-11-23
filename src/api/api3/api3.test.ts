4f321f47e2b770585a8662ef6f045220ee9789e6
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 12:54:33 +1300

    removed api reachability test and created valid input test to check for status code as well as output values, imported app.

--- src/api/api3/api3.test.ts
+++ src/api/api3/api3.test.ts
@@ -2,6 +2,8 @@ const express = require("express");
 import { router } from "./routes/api3Routes";
 import request from "supertest";
 import { describe, test, expect } from "@jest/globals";
+import { response } from "express";
+import app from "../api3/index";
 
 const api = express();
 
@@ -11,14 +13,22 @@ api.use(express.json());
 api.use("/", router);
 
 describe("Qoute Route", () => {
+  test("API should return correct premiums for valid input", async () => {
+    const input = { car_value: 6614.75, risk_rating: 5 };
 
-  test("Api is Reachable: responds with 200 status code", () => {
-    expect(200).toEqual(200);
+    // Send the request to the API
+    const response = await request(app)
+      .post("/quote") // Use POST to send the input data
+      .send(input);
 
+    expect(response.status).toBe(200);
+
+    expect(response.body.yearly_premium).toEqual(330.75);
+    expect(response.body.monthly_premium).toEqual(27.76);
   });
 
-  test("API works as intended: car_value: 6614.75, risk_rating 5", () => {
-    expect({"monthly_premium": 27.56, "yearly_premium": 330.75})
-  })
-
+  test("API should return an error when required fields are missing,", async () => {
+    //missing car value
+    expect(response.status).toBe(400);
+  });
 });
