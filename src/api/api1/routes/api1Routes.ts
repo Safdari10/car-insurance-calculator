bb1327d35a1471b04c2929265c047526f1cba625
Jasmin Benioni <jasminb@missionreadyhq.com>
2024-11-24 10:19:55 +1300

    created and ran failed tests

---
+++ src/api/api1/routes/api1Routes.ts
@@ -0,0 +1,10 @@
+// Defines endpoints and links them to controllers.
+
+import express from 'express';
+
+const router = express.Router();
+
+// Add your routes here, for example:
+// router.get('/example', exampleController);
+
+export const api1Routes = router;
