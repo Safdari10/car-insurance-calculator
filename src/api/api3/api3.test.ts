ff4ea42d020298ea8bc3d237374032f846cfbb40
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 12:19:54 +1300

    Created two test cases for checking api reachabliity and functionality Given valid Input

--- src/api/api3/api3.test.ts
+++ src/api/api3/api3.test.ts
@@ -1,7 +1,7 @@
 const express = require("express");
 import { router } from "./routes/api3Routes";
-import request from "supertest"
-import { describe, test, expect } from "@jest/globals"
+import request from "supertest";
+import { describe, test, expect } from "@jest/globals";
 
 const api = express();
 
@@ -9,3 +9,16 @@ api.use(express.urlencoded({ extended: true }));
 api.use(express.json());
 
 api.use("/", router);
+
+describe("Qoute Route", () => {
+
+  test("Api is Reachable: responds with 200 status code", () => {
+    expect(200).toEqual(200);
+
+  });
+
+  test("API works as intended: car_value: 6614.75, risk_rating 5", () => {
+    expect({"monthly_premium": 27.56, "yearly_premium": 330.75})
+  })
+
+});
