4462a3590a290a4476d7dbaa5460b4c2f10b1ea2
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 14:35:30 +1300

    created basic route handler in controller.ts for api3

--- src/api/api3/controllers/api3Controller.ts
+++ src/api/api3/controllers/api3Controller.ts
@@ -3,3 +3,15 @@ import { Request, Response } from "express"
 export const index = (req: Request, res: Response) => {
     res.status(200).json({ message: "welcome to my api"})
 }
+
+export const quote = (req: Request, res: Response) => {
+    const { car_value, risk_rating } = req.body
+
+    const yearly_premium = car_value * risk_rating / 100
+    const monthly_premium = yearly_premium / 12
+
+    return {
+        yearly_premium: yearly_premium,
+        monthly_premium: monthly_premium
+    }
+}
