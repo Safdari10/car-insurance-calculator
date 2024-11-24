81d7b5b9bd6c73107b0097ca4713821232ee8ad7
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 16:05:20 +1300

    created checks for negative car value and risk rating range and the related error handling

--- src/api/api3/models/api3Model.ts
+++ src/api/api3/models/api3Model.ts
@@ -1,15 +1,21 @@
-
 export const calculatePremiums = (car_value: number, risk_rating: number) => {
-    if(typeof car_value != "number" || typeof risk_rating != "number"){
-        throw new Error("Invalid input")
-    }
+  if (typeof car_value !== "number" || typeof risk_rating !== "number") {
+    throw new Error("Invalid input");
+  }
 
-    const yearly_premium = (car_value * risk_rating) / 100;
-    const monthly_premium = yearly_premium / 12;
-    
-    return {
-        yearly_premium,
-        monthly_premium
-    }
+  if (car_value <= 0) {
+    throw new Error("Car value must be a positive number");
+  }
 
-}
+  if (risk_rating < 1 || risk_rating > 5) {
+    throw new Error("Risk Rating is invalid");
+  }
+
+  const yearly_premium = (car_value * risk_rating) / 100;
+  const monthly_premium = yearly_premium / 12;
+
+  return {
+    yearly_premium,
+    monthly_premium,
+  };
+};
