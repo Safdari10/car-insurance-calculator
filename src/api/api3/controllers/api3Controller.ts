8bf26319c4aaa05a4c7fab89c4a02b736de1b895
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 15:20:54 +1300

    fixed overload error/void error for handler function by setting type to void and removing return statement

--- src/api/api3/controllers/api3Controller.ts
+++ src/api/api3/controllers/api3Controller.ts
@@ -5,17 +5,22 @@ export const index = (req: Request, res: Response) => {
   res.status(200).json({ message: "Welcome to my API" });
 };
 
-export const quote = async (req: Request, res: Response): Promise<Response> => {
+export const quote = (req: Request, res: Response): void => {
   const { car_value, risk_rating } = req.body;
 
   try {
+    // Calculate premiums
     const premiums = calculatePremiums(car_value, risk_rating);
-    return res.status(200).json(premiums);
+
+    // Send the calculated premiums as the response
+    res.status(200).json(premiums);  
   } catch (error) {
     if (error instanceof Error) {
-      return res.status(400).json({ error: error.message });
+      // Handle known error
+      res.status(400).json({ error: error.message });
     } else {
-      return res.status(500).json({ error: "Internal Server Error" });
+      // Handle unexpected errors
+      res.status(500).json({ error: "Internal Server Error" });
     }
   }
 };
