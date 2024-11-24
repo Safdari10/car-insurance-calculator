62a45ce6be2f89dc81363609879d6c646fbf1c27
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 14:48:15 +1300

    refactored controller by importing business logic from the model.

--- src/api/api3/controllers/api3Controller.ts
+++ src/api/api3/controllers/api3Controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express"
+import { calculatePremiums } from "../models/api3Model"
 
 export const index = (req: Request, res: Response) => {
     res.status(200).json({ message: "welcome to my api"})
@@ -7,11 +8,11 @@ export const index = (req: Request, res: Response) => {
 export const quote = (req: Request, res: Response) => {
     const { car_value, risk_rating } = req.body
 
-    const yearly_premium = car_value * risk_rating / 100
-    const monthly_premium = yearly_premium / 12
+   try {
+    const premiums = calculatePremiums(car_value, risk_rating)
+    return res.status(200).json(premiums)
+   } catch (error) {
+    
+   }
 
-    return res.status(200).json({
-        yearly_premium: yearly_premium,
-        monthly_premium: monthly_premium
-    })
 }
