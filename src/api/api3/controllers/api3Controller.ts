7d904defb3e45aa9e8bda09be499b1a282eee59a
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 15:00:47 +1300

    fixed handler function for quote api by making sure we are using async function and also explecitly stating promise type

--- src/api/api3/controllers/api3Controller.ts
+++ src/api/api3/controllers/api3Controller.ts
@@ -5,17 +5,21 @@ export const index = (req: Request, res: Response) => {
   res.status(200).json({ message: "welcome to my api" });
 };
 
-export const quote = (req: Request, res: Response) => {
+export const quote = async (req: Request, res: Response): Promise<Response> => {
   const { car_value, risk_rating } = req.body;
 
   try {
+    // Calculate premiums
     const premiums = calculatePremiums(car_value, risk_rating);
+
+    // Return the calculated premiums
     return res.status(200).json(premiums);
   } catch (error) {
     if (error instanceof Error) {
+      // Handle known error
       return res.status(400).json({ error: error.message });
     } else {
-      // For any unexpected error type, respond with a generic server error message
+      // Handle unexpected errors
       return res.status(500).json({ error: "Internal Server Error" });
     }
   }
