9fec1e31b83289a6f16b202309ff418d05a05aa2
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 14:54:34 +1300

    created error handling in the handler function using try catch

--- src/api/api3/controllers/api3Controller.ts
+++ src/api/api3/controllers/api3Controller.ts
@@ -1,18 +1,22 @@
-import { Request, Response } from "express"
-import { calculatePremiums } from "../models/api3Model"
+import { Request, Response } from "express";
+import { calculatePremiums } from "../models/api3Model";
 
 export const index = (req: Request, res: Response) => {
-    res.status(200).json({ message: "welcome to my api"})
-}
+  res.status(200).json({ message: "welcome to my api" });
+};
 
 export const quote = (req: Request, res: Response) => {
-    const { car_value, risk_rating } = req.body
+  const { car_value, risk_rating } = req.body;
 
-   try {
-    const premiums = calculatePremiums(car_value, risk_rating)
-    return res.status(200).json(premiums)
-   } catch (error) {
-    
-   }
-
-}
+  try {
+    const premiums = calculatePremiums(car_value, risk_rating);
+    return res.status(200).json(premiums);
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message });
+    } else {
+      // For any unexpected error type, respond with a generic server error message
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+};
