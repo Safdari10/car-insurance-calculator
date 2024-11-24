02d0266c243883ba39fcd18c5ac7b68d3a99535b
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 14:06:09 +1300

    Refectored Tests by creating test suit for success cases and looping through each cases in the array. also refectored success cases to check maximum scenarios using minimum test cases

--- src/api/api3/api3.test.ts
+++ src/api/api3/api3.test.ts
@@ -2,7 +2,6 @@ const express = require("express");
 import { router } from "./routes/api3Routes";
 import request from "supertest";
 import { describe, test, expect } from "@jest/globals";
-import { response } from "express";
 import app from "../api3/index";
 
 const api = express();
@@ -12,33 +11,29 @@ api.use(express.json());
 
 api.use("/", router);
 
-describe("Qoute Route", () => {
-  test("API should return correct premiums for valid input", async () => {
-    const input = { car_value: 6614.75, risk_rating: 5 };
+describe("Success cases for /quote API (include edge cases)", () => {
+  const successCases = [
+    {
+      input: { car_value: 5000, risk_rating: 1 },
+      expectedOutput: { monthly_premium: 4.17, yearly_premium: 50 },
+    },
+    {
+      input: { car_value: 100000000, risk_rating: 5 },
+      expectedOutput: { monthly_premium: 416666.67, yearly_premium: 50000 },
+    },
+    {
+      input: { car_value: 0, risk_rating: 1 },
+      expectedOutput: { monthly_premium: 0, yearly_premium: 0 },
+    },
+  ];
 
-    const response = await request(app).post("/quote").send(input);
+  test.each(successCases)(
+    "should return correct premiums for input values",
+    async (input, expectedOutput) => {
+      const response = await request(app).post("/quote").send(input);
 
-    expect(response.status).toBe(200);
-
-    expect(response.body.yearly_premium).toEqual(330.75);
-    expect(response.body.monthly_premium).toEqual(27.76);
-  });
-
-  test("API should return an error when car_value is missing", async () => {
-    const input = { risk_rating: 5 };
-
-    const response = await request(app).post("/quote").send(input);
-
-    expect(response.status).toBe(400);
-    expect(response.body).toEqual({ error: "Missing Input" });
-  });
-
-  test("API should return an error when risk_rating is missing", async () => {
-    const input = { car_value: 6614.75 };
-
-    const response = await request(app).post("/quote").send(input);
-
-    expect(response.status).toBe(400);
-    expect(response.body).toEqual({ error: "Missing Input" });
-  });
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(expectedOutput);
+    }
+  );
 });
