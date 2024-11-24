3587ed0f7d2a17d945c725c990e72161c660b0cb
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 20:12:24 +1300

    adjust business logic return to make sure its rounded to 2 decimal places and made sure its going to be a number

--- src/api/api3/models/api3Model.ts
+++ src/api/api3/models/api3Model.ts
@@ -3,7 +3,7 @@ export const calculatePremiums = (car_value: number, risk_rating: number) => {
   const monthly_premium = yearly_premium / 12;
 
   return {
-      yearly_premium,
-      monthly_premium
+    yearly_premium: Number(yearly_premium.toFixed(2)),
+    monthly_premium: Number(monthly_premium.toFixed(2)),
   };
-}
+};
