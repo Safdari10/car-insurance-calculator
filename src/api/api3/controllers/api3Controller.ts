be0180d5d14659054c1b328d780e338b22754ad9
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 16:17:49 +1300

    moved the validation checks and error handlings to controller

--- src/api/api3/controllers/api3Controller.ts
+++ src/api/api3/controllers/api3Controller.ts
@@ -1,22 +1,32 @@
 import { Request, Response } from "express";
 import { calculatePremiums } from "../models/api3Model";
 
-export const index = (req: Request, res: Response) => {
+export const index = (req: Request, res: Response): void => {
   res.status(200).json({ message: "Welcome to my API" });
 };
 
-export const quote = (req: Request, res: Response): void => {
+export const quote = async (req: Request, res: Response): Promise<void> => {
   const { car_value, risk_rating } = req.body;
+
+  if (typeof car_value !== "number" || typeof risk_rating !== "number") {
+    res.status(400).json({ error: "Invalid input" });
+    return;
+  }
+
+  if (car_value <= 0) {
+    res.status(400).json({ error: "Car value must be a positive number" });
+    return;
+  }
+
+  if (risk_rating <= 0) {
+    res.status(400).json({ error: "Risk Rating must be a positive number" });
+    return;
+  }
 
   try {
     const premiums = calculatePremiums(car_value, risk_rating);
-
     res.status(200).json(premiums);
   } catch (error) {
-    if (error instanceof Error) {
-      res.status(400).json({ error: error.message });
-    } else {
-      res.status(500).json({ error: "Internal Server Error" });
-    }
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
