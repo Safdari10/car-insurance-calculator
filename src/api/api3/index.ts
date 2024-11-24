74f9cf60efe1bfe85518db28d5cff225db8113ff
Zaker Safdari <zakersafdari@gmail.com>
2024-11-24 15:31:07 +1300

    updated node app name to app from api

--- src/api/api3/index.ts
+++ src/api/api3/index.ts
@@ -1,11 +1,10 @@
 const express = require("express");
 import { router } from "./routes/api3Routes";
 
-const api = express();
+const app = express();
 
-api.use(express.urlencoded({ extended: true }));
-api.use(express.json());
+app.use(express.json());
 
-api.use("/api3", router)
+app.use("/api3", router)
 
-export default api
+export default app
