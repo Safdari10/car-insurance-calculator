c4934d4c418cb552eee67ca3f86aeff376b161a0
Zaker Safdari <zakersafdari@gmail.com>
2024-11-27 20:53:22 +1300

    refactored the code by simplifying and joining the test cases into one set

--- src/api/api3/api3.test.ts
+++ src/api/api3/api3.test.ts
@@ -1,75 +1,64 @@
 import request from "supertest";
 import app from "./app";
 
-describe("Success cases for /quote API (include edge cases)", () => {
-  const successCases = [
+describe("Testing POST API /quote", () => {
+  const testCases = [
     {
       input: { car_value: 5000, risk_rating: 1 },
       expectedOutput: { monthly_premium: 4.17, yearly_premium: 50 },
+      expectedStatus: 200,
     },
     {
       input: { car_value: 100000000, risk_rating: 5 },
       expectedOutput: { monthly_premium: 416666.67, yearly_premium: 5000000 },
+      expectedStatus: 200,
     },
     {
-      input : { car_value: "5,000", risk_rating: 1 },
+      input: { car_value: "5,000", risk_rating: 1 },
       expectedOutput: { monthly_premium: 4.17, yearly_premium: 50 },
-    }
-  ];
-
-  test.each(successCases)(
-    "should return correct premiums for input values",
-    async ({ input, expectedOutput }) => {
-      const response = await request(app).post("/quote").send(input);
-
-      expect(response.status).toBe(200);
-      expect(response.body.yearly_premium).toEqual(
-        expectedOutput.yearly_premium,
-       
-      ); 
-      expect(response.body.monthly_premium).toEqual(
-        expectedOutput.monthly_premium,
-        
-      ); 
+      expectedStatus: 200,
     },
-  );
-});
-
-describe("Error cases for /quote API", () => {
-  const errorCases = [
     {
       input: { car_value: 6614, risk_rating: 6 },
-      expectedError: "Risk Rating must be between 1 and 5",
+      expectedOutput: { error: "Risk Rating must be between 1 and 5" },
+      expectedStatus: 400,
     },
     {
       input: { car_value: 6614, risk_rating: 0 },
-      expectedError: "Car Value and Risk Rating are required",
+      expectedOutput: { error: "Car Value and Risk Rating are required" },
+      expectedStatus: 400,
     },
     {
       input: { car_value: 6614 },
-      expectedError: "Car Value and Risk Rating are required",
+      expectedOutput: { error: "Car Value and Risk Rating are required" },
+      expectedStatus: 400,
     },
     {
       input: { risk_rating: 3 },
-      expectedError: "Car Value and Risk Rating are required",
+      expectedOutput: { error: "Car Value and Risk Rating are required" },
+      expectedStatus: 400,
     },
     {
       input: { car_value: "Abc", risk_rating: "#$2" },
-      expectedError: "Car Value and Risk Rating must be valid numbers",
+      expectedOutput: {
+        error: "Car Value and Risk Rating must be valid numbers",
+      },
+      expectedStatus: 400,
     },
     {
       input: { car_value: -5000, risk_rating: 3 },
-      expectedError: "Car Value must be a positive number",
+      expectedOutput: { error: "Car Value must be a positive number" },
+      expectedStatus: 400,
     },
   ];
 
-  test.each(errorCases)(
-    "should return 400 for invalid input",
-    async ({ input, expectedError }) => {
+  test.each(testCases)(
+    "should return return expected output given the inputs",
+    async ({ input, expectedOutput, expectedStatus }) => {
       const response = await request(app).post("/quote").send(input);
-
-      expect(response.status).toBe(400);
-      expect(response.body).toEqual({ error: expectedError });
+      const body = response.body;
+      expect(response.status).toBe(expectedStatus);
+      expect(response.body).toEqual(expectedOutput);
     }
   );
 });
