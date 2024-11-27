50215e3592b2c8091c695bcd06b32d9a8e56f4cf
Zaker Safdari <zakersafdari@gmail.com>
2024-11-27 13:20:27 +1300

    added controller api 2

--- src/api/api2/Controllers/api2Controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express';
-import { calculateRiskRating } from '../models/api2Model';
-
-export const getRiskRating = (req: Request, res: Response): void => {
-  const { claim_history } = req.body;
-  const result = calculateRiskRating(claim_history);
-  res.json(result);
