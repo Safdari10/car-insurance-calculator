a28dccd3c3acc5367b73976543d859424958bc7a
Jasmin Benioni <jasminb@missionreadyhq.com>
2024-11-28 12:55:22 +1300

    renamed files

--- src/api/api2/api2.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import riskRatingRoutes from './routes/api2Routes';
-
-const app = express();
-app.use(express.json()); 
-
-app.use('/api2', riskRatingRoutes);
-
-export default app;
--- src/api/api2/routes/api2Routes_original.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getRiskRating } from '../Controllers/api2Controller';
-
-const router = Router();
-
-router.post('/risk-rating', getRiskRating);
-
-export default router;
